62fe483d6c522573159f6df545f6b7b076a8dc01
hyein0608 <hyein0608sa@gmail.com>
2023-05-16 16:32:39 +0900

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,11 @@
 function Menu () {
-    
+    return (
+        <div id="menu">
+            <div>OOO님, 안녕하세요!</div>
+            <div>상품 리스트 페이지</div>
+            <div>북마크 페이지</div>
+        </div>
+    )
 }
+
+export default Menu
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import Menu from './Menu';
 
 function Nav() {
-    
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav>
             <img id="logo" src="../로고.png" alt="logo"></img>
             <h1>COZ Shopping</h1>
-            <MenuRoundedIcon id="menu-icon"></MenuRoundedIcon>
+            <MenuRoundedIcon id="menu-icon" onClick={() => {
+                setIsOpen(!isOpen)
+            }}>
+            </MenuRoundedIcon>
+            {isOpen ? <Menu /> : null}
         </nav>
     )
-
 }
 
 export default Nav;
