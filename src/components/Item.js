467b9e815c4f3302eb3560fbfedd16e44dd71a7c
Yirey Suh <yrysuh@gmail.com>
2023-05-17 20:16:26 +0900

    fix item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
+
+import Star from '../components/Star';
 import Close from './Vector.png';
 
-const Item = ({ data }) => {
-  const { title, sub_title, brand_name, discountPercentage, price, image_url, brand_image_url, follower } = data;
+// todo: use modal
+
+const Item = ({ data, isBookmark }) => {
+  const {
+    title,
+    sub_title,
+    brand_name,
+    discountPercentage,
+    price,
+    image_url,
+    brand_image_url,
+    follower,
+    type
+  } = data;
   const [show, setShow] = useState(false);
+
+  let realTitle = type === 'Category' ? `# ${title}` : title || brand_name;
   return (
     <>
       <div className="item-box">
@@ -13,27 +29,43 @@ const Item = ({ data }) => {
             setShow(true);
           }}
         >
-          <img src={image_url ? image_url : brand_image_url} alt='img'/>
+          <div className="item-box-upper-area">
+            <img src={image_url ? image_url : brand_image_url} alt="img" />
+            <Star className={'star-item' + (isBookmark ? ' liked' : '')} />
+          </div>
         </div>
 
         <div className="item-box-lower">
           <div className="item-box-title">
-            <div>{title ? title : brand_name}</div>
-            {discountPercentage ? <div>{`${discountPercentage}%`}</div> : null}
+            <div>{realTitle}</div>
+            {discountPercentage ? (
+              <div className="percent-text">{`${discountPercentage}%`}</div>
+            ) : null}
             {follower ? <div>관심고객수</div> : null}
           </div>
-          <div className="item-box-percent">{price ? price : follower}</div>
+          <div className="item-box-percent">
+            {price ? `${price}원` : follower}
+          </div>
           <div className="item-box-subtitle">{sub_title}</div>
         </div>
       </div>
       {show ? (
-        <div
-          className="image-modal"  
-        >
+        <div className="image-modal">
           <div className="image-modal-box">
-            <img src={image_url ? image_url : brand_image_url} alt='img' />
-            <div className='close' onClick={() => { setShow(false) }}>
-              <img src={Close} alt='img'></img>
+            <img src={image_url ? image_url : brand_image_url} alt="img" />
+            <div className="star-modal-area">
+              <div
+                className="close"
+                onClick={() => {
+                  setShow(false);
+                }}
+              >
+                <img src={Close} alt="img"></img>
+              </div>
+              <div className="star-modal-area-text">
+                <Star className={'star-modal'} isBookmark={isBookmark} />
+                <span className="">{realTitle}</span>
+              </div>
             </div>
           </div>
         </div>
